c38f464ad7f06f308894a8af03b3a415853cd096
elonehoo <hi@elonehoo.me>
2023-03-13 19:36:45 +0800

    fix: init test

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vitest/config';
+import GitHubVitest from './src';
+// Import { GithubReporter } from './dist/index.mjs';
+
+export default defineConfig({
+  test:{
+    reporters: process.env.GITHUB_ACTIONS
+            ? ['default', new GitHubVitest()]
+            : 'default',
+  }
+});
